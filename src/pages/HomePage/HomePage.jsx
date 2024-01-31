9da59a6bc0d68a5d7529fbf5133d722a28ef4d5b
Ngan LE <ngan.ltkn2911@gmail.com>
2024-01-31 23:08:13 +0100

    slider for homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import CoursesList from '../../components/CoursesList/CoursesList'
+import SliderHome from '../../components/SliderHome/SliderHome'
 
 export default function HomePage() {
   return (
     <div className='homePage py-24'>
+        <SliderHome />
         <CoursesList />
     </div>
   )
