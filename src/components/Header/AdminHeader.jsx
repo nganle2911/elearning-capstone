c9e55b1e18d493526c6333ac2ba887496d4b5cd2
Ngan LE <ngan.ltkn2911@gmail.com>
2024-02-19 23:20:57 +0100

    execute login for admin page

--- src/components/Header/AdminHeader.jsx
+++ src/components/Header/AdminHeader.jsx
@@ -2,13 +2,60 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { ButtonStyled } from '../ButtonStyled/ButtonStyled'
 import { useSelector } from 'react-redux'
+import { Avatar, Dropdown } from 'antd';
+import { RANDOM_NUM } from '../../services/constant';
+import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
 
 export default function AdminHeader() {
     const { user } = useSelector(state => state.userSlice);
+
+    const items = [
+        {
+            label: (<a className='text-lg'>
+                <UserOutlined className='mr-2' />
+                {user.hoTen}
+            </a>),
+            key: '1',
+        },
+        {
+            label: (<a className='text-lg'>
+                <LogoutOutlined className='mr-2' />
+                Đăng xuất
+            </a>),
+            key: '3',
+        },
+    ];
+
+    // todo: render admin account
+    const renderAdminAccount = () => {
+        if (user) {
+            return (
+                <>
+                    <div className='item__avatar' style={{width: '55px', height: '55px', backgroundColor: 'rgba(29, 123, 133, 0.3)', borderRadius: '50%', cursor: 'pointer', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        <Dropdown 
+                            menu={{items}}
+                            trigger={['click']}
+                        >
+                            <Avatar size={48} src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${RANDOM_NUM}`} />
+                        </Dropdown>
+                    </div>
+                </>
+            )
+        } else {
+            return (
+                <div className='headAdmin__item'>
+                    <NavLink to={"/login"}>
+                        <ButtonStyled>Log in</ButtonStyled>
+                    </NavLink>
+                </div>
+            )
+        }
+    }
     
     return (
         <header className='headerAdmin bg-white w-full h-24 fixed z-20 drop-shadow'>
             <nav className='container py-2 flex items-center justify-between' aria-label="Global">
+                {/* logo */}
                 <div>
                     <NavLink to={"/admin"}>
                         <img className='w-52' src='../../img/logo.png' alt='logo-edu' />
@@ -16,11 +63,7 @@ export default function AdminHeader() {
                 </div>
 
                 {/* Account */}
-                <div className='headAdmin__item'>
-                    <NavLink to={"/login"}>
-                        <ButtonStyled>Log in</ButtonStyled>
-                    </NavLink>
-                </div>
+                {renderAdminAccount()}
             </nav>
         </header>
     )
