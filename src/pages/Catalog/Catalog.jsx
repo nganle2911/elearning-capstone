64933fee516b25599ee32c9dcb2b3ccc483e01f0
Ngan LE <ngan.ltkn2911@gmail.com>
2024-02-20 16:58:13 +0100

    add detail course

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -4,19 +4,19 @@ import { https } from '../../services/api'
 import Course from '../../components/Course/Course'
 
 export default function Catalog() {
-    const {maDanhMuc} = useParams({})
+    const { maDanhMuc } = useParams({})
     const [catalog, setCatalog] = useState([])
 
     useEffect(() => {
         https.get(`api/QuanLyKhoaHoc/LayKhoaHocTheoDanhMuc?maDanhMuc=${maDanhMuc}&MaNhom=GP09`)
-        .then((res) => {
-            console.log(res.data)
-         setCatalog(res.data)
-        })
-        .catch((err) => {
-         console.log(err);
-         });
-    },[maDanhMuc])
+            .then((res) => {
+                console.log(res.data)
+                setCatalog(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [maDanhMuc])
 
     const renderCoursesList = () => {
         return catalog.slice(0, 12).map((course, index) => {
@@ -29,7 +29,7 @@ export default function Catalog() {
             )
         })
     }
-  
+
     const renderTitleCatalog = () => {
         let renderTitle = '';
 
@@ -45,24 +45,25 @@ export default function Catalog() {
             return null;
         })
     }
-  return (
-    <div>
-        <div className='homePage__courses container'>
-            <div className='catalogCourses'>
-            <h1>KHÓA HỌC THEO DANH MỤC</h1>
-            <p>HÃY CHỌN KHÓA HỌC MONG MUỐN!!!</p>
+
+    return (
+        <div>
+            <div className='homePage__courses container'>
+                <div className='catalogCourses'>
+                    <h1>KHÓA HỌC THEO DANH MỤC</h1>
+                    <p>HÃY CHỌN KHÓA HỌC MONG MUỐN!!!</p>
+                </div>
+                <button className='catalog_title'>
+                    <div className='flex justify-center items-center space-x-1'>
+                        <i class="fa fa-desktop"></i>
+                        {renderTitleCatalog()}
+                    </div>
+                </button>
+                <div className='courses__list grid grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 py-10'>
+                    {renderCoursesList()}
+                </div>
             </div>
-        <button className='catalog_title'>
-            <div className='flex justify-center items-center space-x-1'>
-            <i class="fa fa-desktop"></i>
-            {renderTitleCatalog()}
-            </div>
-        </button>
-        <div className='courses__list grid grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 py-10'>
-        {renderCoursesList()}
         </div>
-        </div>  
-    </div>
-    
-  )
+
+    )
 }
