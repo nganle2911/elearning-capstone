8d275f29cd155a2bcb40731faeb31c18173e8b3a
Ngan LE <ngan.ltkn2911@gmail.com>
2024-02-21 11:31:40 +0100

    add secure gate

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Layout, Result, Tabs, theme } from 'antd';
 import CourseManagement from './CourseManagement/CourseManagement';
 import UserMgmt from './UserManagement/UserMgmt';
-import { useSelector } from 'react-redux';
-import { ButtonStyled } from '../../components/ButtonStyled/ButtonStyled';
-import { NavLink } from 'react-router-dom';
 const { Content } = Layout;
 
 const items = [
@@ -25,35 +22,6 @@ const onChange = (key) => {
 };
 
 export default function AdminPage() {
-    const { user } = useSelector(state => state.userSlice);
-
-    // todo: render admin content depending on type of user
-    const renderAdContent = () => {
-        if (user) {
-            if (user.maLoaiNguoiDung === "GV") {
-                return (
-                    <>
-                        <Tabs className='adPage__tabs md:hidden' defaultActiveKey='1' items={items} onChange={onChange} tabPosition='left' />
-
-                        {/* Tabs for mobile screen */}
-                        <Tabs className='adPageMobile__tabs mx-6 hidden md:block' defaultActiveKey='1' items={items} onChange={onChange} tabPosition="top" />
-                    </>
-                );
-            } else {
-                return (
-                    <Result
-                        status="403"
-                        title="403"
-                        subTitle="Xin lỗi, bạn không được phép truy cập trang này!"
-                        extra={<NavLink to={"/"}>
-                            <ButtonStyled>Về trang chủ</ButtonStyled>
-                        </NavLink>}
-                    />
-                );
-            }
-        }
-    }
-
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
@@ -75,7 +43,10 @@ export default function AdminPage() {
                         borderRadius: borderRadiusLG,
                     }}
                 >
-                    {renderAdContent()}
+                    <Tabs className='adPage__tabs md:hidden' defaultActiveKey='1' items={items} onChange={onChange} tabPosition='left' />
+
+                    {/* Tabs for mobile screen */}
+                    <Tabs className='adPageMobile__tabs mx-6 hidden md:block' defaultActiveKey='1' items={items} onChange={onChange} tabPosition="top" />
                 </Layout>
             </Content>
         </Layout>
