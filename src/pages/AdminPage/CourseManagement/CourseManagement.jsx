84bc71f4f2b55fe55eae41538ed965e865302a0f
KHA0410 <khanguyen151121@gmail.com>
2024-02-21 19:44:19 +0700

    courseAdmin

--- src/pages/AdminPage/CourseManagement/CourseManagement.jsx
+++ src/pages/AdminPage/CourseManagement/CourseManagement.jsx
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { Space, Table, Tag, message } from 'antd';
+import { Button, Space, Table, Tag, message } from 'antd';
 import { https } from '../../../services/api';
 import { ButtonStyled } from '../../../components/ButtonStyled/ButtonStyled';
+import Modal from 'antd/es/modal/Modal';
+import confirm from 'antd/es/modal/confirm';
+import FormAddCourse from './FormAddCourse';
 
 export default function CourseManagement() {
   const [listCourse, setListCourse] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   let fetchCourseList = () => {
-    https.get("api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP02")
+    https.get("api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP01")
       .then((res) => {
         console.log("danh sách khóa học nè", res.data);
         setListCourse(res.data)
@@ -21,25 +34,40 @@ export default function CourseManagement() {
     fetchCourseList()
   }, []);
 
-  // let handleDelete = (id) => {
-  //     https.delete(`api/QuanLyKhoaHoc/XoaKhoaHoc?MaKhoaHoc=${id}`)
-  //     .then((res) => {
-  //      console.log("delete",res);
-  //      message.success("Delete Success")
-  //     })
-  //     .catch((err) => {
-  //      console.log(err);
-  //      message.error("Delete Erorr")
-  //      });
-  // }
+  let showConfirm = () => {
+    confirm({
+        title: 'Đặt vé thành công !',
+        content: <FormAddCourse/>,
+        onOk(){
+          <div>
+            <h1>sd</h1>
+          </div>},
+        onCancel(){
+          <div>
+            <h1>sd</h1>
+          </div>
+        }
+    })
+}
+
+  let handleAdd = async() => {
+    try{
+      let res = await https.post("api/QuanLyKhoaHoc/ThemKhoaHoc")
+      message.success("Thêm thành công")
+    }
+    catch(erorr){
+      message.error("Thêm thất bại")
+    }
+  }
+
   let handleDelete = async (id) => {
     try {
       await https.delete(`api/QuanLyKhoaHoc/XoaKhoaHoc?MaKhoaHoc=${id}`)
-      message.success("Delete Success")
+      message.success("xóa thành công")
       fetchCourseList()
     }
     catch (error) {
-      message.error("Delete Erorr")
+      message.error(error.response.data)
     }
   }
   const columns = [
@@ -75,12 +103,38 @@ export default function CourseManagement() {
       title: 'Điều chỉnh',
       key: 'action',
       render: (_, record) => (
-        <button onClick={() => { handleDelete(record.maKhoaHoc) }}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" className="w-6 h-6 text-red-400">
+        <Space>
+          {/* Thêm  */}
+        <div>
+           <Button  onClick={showModal}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-400">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+        </svg>
+        </Button>
+        <Modal colorBgMask="rgba(0, 0, 0, 0.45)" onClick={showModal} title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <FormAddCourse/>
+        </Modal>
+        </div>
+       
+        {/* XÓA */}
+        <Button onClick={showConfirm}>
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-yellow-400">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+          </svg> 
+        </Button>
+        
+        {/* SỬA */}
+        <Button>
+          <svg onClick={() => { handleDelete(record.maKhoaHoc) }}
+        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" className="w-6 h-6 text-red-400">
             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
           </svg>
-        </button>
+        </Button>
+        
+
+        </Space>
       ),
+      
     },
   ];
   return (
