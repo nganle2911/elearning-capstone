875f4d044f9759dd8caa0e42d5f03fa85a06a52d
Ngan LE <ngan.ltkn2911@gmail.com>
2024-02-23 17:27:50 +0100

    style icon for user admin

--- src/pages/AdminPage/CourseManagement/CourseManagement.jsx
+++ src/pages/AdminPage/CourseManagement/CourseManagement.jsx
@@ -41,18 +41,19 @@ export default function CourseManagement() {
   let AddConfirm = () => {
     //Close modal
     const handleClose = () => {
-      Modal.destroyAll(); 
+      Modal.destroyAll();
     };
+
     confirm({
-        title: <div className='flex justify-between items-center'>
-          <h1 className='titleFuntion space-x-5'>THÊM KHÓA HỌC</h1>
-          <Button onClick={handleClose} className="bg-red-500 text-white"><i class="fa fa-times"></i></Button>
-        </div> ,
-        content: <FormAddCourse/>,
-        okButtonProps: { style: { display: 'none' } },
-        width: '50%'
+      title: <div className='flex justify-between items-center'>
+        <h1 className='titleFuntion space-x-5'>THÊM KHÓA HỌC</h1>
+        <Button onClick={handleClose} className="bg-red-500 text-white"><i class="fa fa-times"></i></Button>
+      </div>,
+      content: <FormAddCourse />,
+      okButtonProps: { style: { display: 'none' } },
+      width: '50%'
     })
-}
+  }
 
   const columns = [
     {
@@ -82,34 +83,34 @@ export default function CourseManagement() {
       key: 'nguoiTao',
       render: (key) => <p>{key.hoTen}</p>
     },
-    
+
     {
       title: 'Điều chỉnh',
       key: 'action',
       render: (_, record) => (
-        <Space size="large">  
+        <Space size="large">
           {/* Duyệt  */}
           <Button>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-400">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
-          </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-400">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+            </svg>
           </Button>
-        
-       
-        {/* SỬA */}
-        <Button>
-         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-yellow-400">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-          </svg> 
-        </Button>
-        
-        {/* XÓA */}
-        <Button>
-          <svg onClick={() => { handleDelete(record.maKhoaHoc) }}
-        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" className="w-6 h-6 text-red-400">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-          </svg>
-        </Button>
+
+
+          {/* SỬA */}
+          <Button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-yellow-400">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg>
+          </Button>
+
+          {/* XÓA */}
+          <Button>
+            <svg onClick={() => { handleDelete(record.maKhoaHoc) }}
+              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" className="w-6 h-6 text-red-400">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>
+          </Button>
         </Space>
       ),
     },
--- src/pages/AdminPage/CourseManagement/FormAddCourse.jsx
+++ src/pages/AdminPage/CourseManagement/FormAddCourse.jsx
@@ -14,21 +14,21 @@ import { ButtonStyled } from '../../../components/ButtonStyled/ButtonStyled';
 import { https } from '../../../services/api';
 
 const FormAddCourse = () => {
-  
+
   let dataJson = JSON.parse(localStorage.getItem("USER_LOGIN"))
   const { TextArea } = Input;
 
   const [danhMuc, setDanhMuc] = useState([])
   useEffect(() => {
     https.get("api/QuanLyKhoaHoc/LayDanhMucKhoaHoc")
-    .then((res) => {
-     console.log(res.data);
-     setDanhMuc(res.data)
-    })
-    .catch((err) => {
-     console.log(err);
-     });
-  },[])
+      .then((res) => {
+        console.log(res.data);
+        setDanhMuc(res.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [])
 
   //Chuyển đổi tên file hình ảnh
   const normFile = (e) => {
@@ -37,15 +37,15 @@ const FormAddCourse = () => {
     }
     const fileList = e && e.fileList;
     const fileName = fileList && fileList.length > 0 && fileList[0].name;
-  
-    return fileName; 
+
+    return fileName;
   };
 
   const onFinish = (values) => {
     console.log('Success:', values);
     https.post("/api/QuanLyKhoaHoc/ThemKhoaHoc", values)
       .then((res) => {
-        console.log("Thêm khóa học",res.data);
+        console.log("Thêm khóa học", res.data);
         message.success("Thêm thành công!")
       })
       .catch((err) => {
@@ -56,6 +56,8 @@ const FormAddCourse = () => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
+
+  
   return (
     <>
       <Form
@@ -77,172 +79,172 @@ const FormAddCourse = () => {
         autoComplete="off"
       >
         <div className='grid grid-cols-2 lg:grid-cols-1 gap-5'>
-        <div>
-          {/* Mã khóa học */}
-        <Form.Item 
-        label="Mã khóa học"
-        name="maKhoaHoc"
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng nhập mã khóa học!',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        {/*Tên khóa học */}
-        <Form.Item 
-        label="Tên khóa học"
-        name="tenKhoaHoc"
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng nhập tên khóa học!',
-            },
-          ]}
-        >
-          <Input/>
-        </Form.Item>
-        {/* Danh mục khóa học */}
-        <Form.Item 
-        label="Danh mục khóa học"
-        name="maDanhMucKhoaHoc"
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng chọn danh mục khóa học!',
-            },
-          ]}
-        >
-          <Select>
-            {danhMuc.map((course, index) => {
-              return <Select.Option value={course.maDanhMuc}>{course.tenDanhMuc}</Select.Option>
-              
-            })}
-          </Select>
-        </Form.Item>
-         {/* Mã nhóm học*/}
-         <Form.Item 
-        label="Mã nhóm học"
-        name="maNhom"
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng chọn nhóm học!',
-            },
-          ]}
-        >
-          <Select>
-          <Select.Option value="GP01">GP01</Select.Option>
-          <Select.Option value="GP02">GP02</Select.Option>
-          <Select.Option value="GP03">GP03</Select.Option>
-          <Select.Option value="GP04">GP04</Select.Option>
-          <Select.Option value="GP05">GP05</Select.Option>
-          <Select.Option value="GP06">GP06</Select.Option>
-          <Select.Option value="GP07">GP07</Select.Option>
-          <Select.Option value="GP08">GP08</Select.Option>
-          <Select.Option value="GP09">GP09</Select.Option>
-          <Select.Option value="GP10">GP10</Select.Option>
-          <Select.Option value="GP11">GP11</Select.Option>
-          <Select.Option value="GP12">GP12</Select.Option>
-          <Select.Option value="GP13">GP13</Select.Option>
-          <Select.Option value="GP14">GP14</Select.Option>
-          <Select.Option value="GP15">GP15</Select.Option>
-          </Select>
-        </Form.Item>
-        {/* Nguời tạo */}
-        <Form.Item 
-        label="Người tạo"
-        name="taiKhoanNguoiTao"
-          rules={[
-            {
-              required: true,
-              message: 'Người tạo phải là GV!',
-            },
-          ]}
-        >
-          <Select>
-            <Select.Option value={dataJson.taiKhoan}>GV</Select.Option>
-          </Select>
-  
-        </Form.Item>
+          <div>
+            {/* Mã khóa học */}
+            <Form.Item
+              label="Mã khóa học"
+              name="maKhoaHoc"
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập mã khóa học!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            {/*Tên khóa học */}
+            <Form.Item
+              label="Tên khóa học"
+              name="tenKhoaHoc"
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập tên khóa học!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            {/* Danh mục khóa học */}
+            <Form.Item
+              label="Danh mục khóa học"
+              name="maDanhMucKhoaHoc"
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng chọn danh mục khóa học!',
+                },
+              ]}
+            >
+              <Select>
+                {danhMuc.map((course, index) => {
+                  return <Select.Option value={course.maDanhMuc}>{course.tenDanhMuc}</Select.Option>
+
+                })}
+              </Select>
+            </Form.Item>
+            {/* Mã nhóm học*/}
+            <Form.Item
+              label="Mã nhóm học"
+              name="maNhom"
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng chọn nhóm học!',
+                },
+              ]}
+            >
+              <Select>
+                <Select.Option value="GP01">GP01</Select.Option>
+                <Select.Option value="GP02">GP02</Select.Option>
+                <Select.Option value="GP03">GP03</Select.Option>
+                <Select.Option value="GP04">GP04</Select.Option>
+                <Select.Option value="GP05">GP05</Select.Option>
+                <Select.Option value="GP06">GP06</Select.Option>
+                <Select.Option value="GP07">GP07</Select.Option>
+                <Select.Option value="GP08">GP08</Select.Option>
+                <Select.Option value="GP09">GP09</Select.Option>
+                <Select.Option value="GP10">GP10</Select.Option>
+                <Select.Option value="GP11">GP11</Select.Option>
+                <Select.Option value="GP12">GP12</Select.Option>
+                <Select.Option value="GP13">GP13</Select.Option>
+                <Select.Option value="GP14">GP14</Select.Option>
+                <Select.Option value="GP15">GP15</Select.Option>
+              </Select>
+            </Form.Item>
+            {/* Nguời tạo */}
+            <Form.Item
+              label="Người tạo"
+              name="taiKhoanNguoiTao"
+              rules={[
+                {
+                  required: true,
+                  message: 'Người tạo phải là GV!',
+                },
+              ]}
+            >
+              <Select>
+                <Select.Option value={dataJson.taiKhoan}>GV</Select.Option>
+              </Select>
+
+            </Form.Item>
+          </div>
+          <div>
+            {/* Ngày tạo */}
+            <Form.Item
+              label="Ngày tạo"
+              name="ngayTao"
+              rules={[
+                {
+                  required: true,
+                  message: 'Ngày tạo không được để trống!',
+                },
+              ]}
+            >
+              <DatePicker />
+            </Form.Item>
+            {/* Đánh giá */}
+            <Form.Item
+              label="Đánh giá"
+              name="danhGia"
+              rules={[
+                {
+                  required: true,
+                  message: 'Đánh giá không được để trống!',
+                },
+              ]}
+            >
+              <InputNumber />
+            </Form.Item>
+            {/* Lượt xem */}
+            <Form.Item
+              style={{
+                width: "100%",
+              }}
+              label="Lượt xem"
+              name="luotXem"
+              rules={[
+                {
+                  required: true,
+                  message: 'Lượt xem không được để trống!',
+                },
+              ]}
+            >
+              <InputNumber />
+            </Form.Item>
+            {/* Mô tả */}
+            <Form.Item
+              label="Mô tả khóa học"
+              name="moTa"
+              rules={[
+                {
+                  required: true,
+                  message: 'Mô tả không được để trống!',
+                },
+              ]}
+            >
+              <TextArea rows={4} />
+            </Form.Item>
+            {/* Hình ảnh */}
+            <Form.Item
+              label="Hình ảnh khóa học"
+              name="hinhAnh"
+              getValueFromEvent={normFile}
+            >
+              <Upload
+                action="/api/upload/image"
+                listType="picture"
+              >
+                <Button icon={<UploadOutlined />}>Tải lên</Button>
+              </Upload>
+            </Form.Item>
+          </div>
         </div>
-        <div>
-           {/* Ngày tạo */}
-        <Form.Item 
-        label="Ngày tạo"
-        name="ngayTao"
-          rules={[
-            {
-              required: true,
-              message: 'Ngày tạo không được để trống!',
-            },
-          ]}
-        >
-          <DatePicker />
-        </Form.Item>
-        {/* Đánh giá */}
-        <Form.Item 
-        label="Đánh giá"
-        name="danhGia"
-          rules={[
-            {
-              required: true,
-              message: 'Đánh giá không được để trống!',
-            },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-        {/* Lượt xem */}
-        <Form.Item 
-        style={{
-          width: "100%",
-        }}
-        label="Lượt xem"
-        name="luotXem"
-          rules={[
-            {
-              required: true,
-              message: 'Lượt xem không được để trống!',
-            },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-        {/* Mô tả */}
-        <Form.Item 
-        label="Mô tả khóa học"
-        name="moTa"
-          rules={[
-            {
-              required: true,
-              message: 'Mô tả không được để trống!',
-            },
-          ]}
-        >
-          <TextArea rows={4} />
-        </Form.Item>
-        {/* Hình ảnh */}
-        <Form.Item
-        label="Hình ảnh khóa học"
-        name="hinhAnh"
-        getValueFromEvent={normFile}
-        >
-          <Upload
-          action="/api/upload/image"
-          listType="picture"
-          >
-          <Button icon={<UploadOutlined />}>Tải lên</Button>
-          </Upload>
-        </Form.Item>
-        </div>
-        </div>
-        
+
         <ButtonStyled className=" text-white w-full font-bold text-xl items-center" htmlType="submit">
-            Thêm
-          </ButtonStyled>
+          Thêm
+        </ButtonStyled>
       </Form>
     </>
   );
