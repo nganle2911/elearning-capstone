27b3e707cf070f383d91570d57eef1766ac82f6c
Ngan LE <ngan.ltkn2911@gmail.com>
2024-02-29 00:04:27 +0100

    add validation for add new user admin

--- src/pages/AdminPage/UserManagement/AddNewUser.jsx
+++ src/pages/AdminPage/UserManagement/AddNewUser.jsx
@@ -34,31 +34,89 @@ export default function AddNewUser() {
         formik.setFieldValue("maLoaiNguoidung", val);
     }
 
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
         <Form
             layout='vertical'
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                offset: 0,
+                span: 24,
+            }}
+            style={{
+                maxWidth: 500,
+            }}
             className='modal__addUser'
             initialValues={{
                 remember: true
             }}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
             onSubmitCapture={formik.handleSubmit}
         >
-            <Form.Item label="Tài khoản">
+            <Form.Item label="Tài khoản" rules={[
+                {
+                    required: true,
+                    message: 'Vui lòng nhập tài khoản!',
+                },
+            ]}>
                 <Input placeholder='Nhập tài khoản' className='h-10' name='taiKhoan' value={formik.values.taiKhoan} onChange={formik.handleChange} />
             </Form.Item>
-            <Form.Item label="Mật khẩu">
+            <Form.Item label="Mật khẩu" rules={[
+                {
+                    required: true,
+                    message: 'Vui lòng nhập mật khẩu!',
+                },
+                {
+                    min: 6,
+                    message: 'Mật khẩu phải có ít nhất 6 ký tự!'
+                }
+            ]}>
                 <Input placeholder='Nhập mật khẩu' className='h-10' name='matKhau' value={formik.values.matKhau} onChange={formik.handleChange} />
             </Form.Item>
-            <Form.Item label="Họ tên">
+            <Form.Item label="Họ tên" rules={[
+                {
+                    required: true,
+                    message: 'Vui lòng nhập họ tên!',
+                },
+            ]}>
                 <Input placeholder='Nhập họ tên' className='h-10' name='hoTen' value={formik.values.hoTen} onChange={formik.handleChange} />
             </Form.Item>
-            <Form.Item label="Số điện thoại">
+            <Form.Item label="Số điện thoại" rules={[
+                {
+                    required: true,
+                    message: 'Vui lòng nhập số điện thoại!',
+                },
+                {
+                    pattern: /^(?:\d\s?){10,12}$/,
+                    message: 'Số điện thoại không hợp lệ!'
+                }
+            ]}>
                 <Input placeholder='Nhập số điện thoại' className='h-10' name='soDT' value={formik.values.soDT} onChange={formik.handleChange} />
             </Form.Item>
-            <Form.Item label="Email">
+            <Form.Item label="Email" rules={[
+                {
+                    required: true,
+                    message: 'Vui lòng nhập email!',
+                },
+                {
+                    type: 'email',
+                    message: 'Email không hợp lệ!'
+                }
+            ]}>
                 <Input placeholder='Nhập email' className='h-10' name='email' value={formik.values.email} onChange={formik.handleChange} />
             </Form.Item>
-            <Form.Item label="Loại">
+            <Form.Item label="Loại" rules={[
+                {
+                    required: true,
+                    message: 'Vui lòng chọn mã nhóm!',
+                },
+            ]}>
                 <Select 
                     className='h-10' 
                     placeholder="Chọn loại người dùng" 
