0afce1ba5e1c9830e1d1ae3e0ab1e2fdf7e9797a
Ngan LE <ngan.ltkn2911@gmail.com>
2024-03-01 00:09:56 +0100

    authenticate course for user

--- src/pages/AdminPage/UserManagement/UserEnrollment.jsx
+++ src/pages/AdminPage/UserManagement/UserEnrollment.jsx
@@ -1,4 +1,4 @@
-import { Button, Pagination, Select, Space, Table, message } from 'antd'
+import { Button, Select, Space, Table, message } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { ButtonStyled } from '../../../components/ButtonStyled/ButtonStyled'
 import { https } from '../../../services/api';
@@ -66,6 +66,18 @@ export default function UserEnrollment({ data }) {
         });
     }
 
+    // todo: authenticate course for user 
+    const authCourse = (course, account) => {
+        https.post("/api/QuanLyKhoaHoc/GhiDanhKhoaHoc", {"maKhoaHoc": course.maKhoaHoc, "taiKhoan": account}).then((res) => {
+            message.success("Ghi danh thành công!");
+            getEnrolledCourses(account);
+            getAwaitingCourses(account);
+        }).catch((err) => {
+            console.log("err", err);
+            message.error(err.response.data);
+        });
+    }
+
     // columns for awaiting courses
     const columnsAwait = [
         {
@@ -87,7 +99,7 @@ export default function UserEnrollment({ data }) {
                 <>
                     <Space size="large">
                         {/* Authentication */}
-                        <Button>
+                        <Button onClick={() => { authCourse(record, data.taiKhoan) }}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-blue-500">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
                             </svg>
