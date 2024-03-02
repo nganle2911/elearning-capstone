f8ec6b831d66802145b0cde6d66bd6959db175d5
KHA0410 <khanguyen151121@gmail.com>
2024-03-02 20:04:42 +0700

    add register and cancelCourse

--- src/pages/Profile/EnrolledCourse/EnrolledCourse.jsx
+++ src/pages/Profile/EnrolledCourse/EnrolledCourse.jsx
@@ -1,57 +1,85 @@
-import { List, Progress, Rate } from 'antd'
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-import { RANDOM_NUM } from '../../../services/constant';
+import { Button, List, Progress, Rate, message } from "antd";
+import React, { useEffect } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import { RANDOM_NUM } from "../../../services/constant";
+import { ButtonStyled } from "../../../components/ButtonStyled/ButtonStyled";
+import { https } from "../../../services/api";
 
 export default function EnrolledCourse({ enrolledCourses }) {
-    // console.log("courses: ", enrolledCourses);
+  console.log("courses: ", enrolledCourses);
+  let dataJson = JSON.parse(localStorage.getItem("USER_LOGIN"));
+  let cancelCourse = (maKhoaHoc) => {
+    https
+      .post("/api/QuanLyKhoaHoc/HuyGhiDanh", {
+        maKhoaHoc: maKhoaHoc,
+        taiKhoan: dataJson.taiKhoan,
+      })
+      .then((res) => {
+        console.log(res);
+        message.success("Hủy thành công");
+        setTimeout(function () {
+          window.location.reload();
+        }, 500);
+      })
+      .catch((err) => {
+        console.log(err);
+        message.error(err.response.data);
+      });
+  };
 
+  const arrCourses = enrolledCourses.map((course, index) => {
+    return {
+      key: index,
+      idCourse: `${course.maKhoaHoc}`,
+      title: `${course.tenKhoaHoc}`,
+      description: `${course.moTa}`,
+      img: `${course.hinhAnh}`,
+      rating: `${course.danhGia}`,
+    };
+  });
 
-    const arrCourses = enrolledCourses.map((course, index) => {
-        return ({
-            key: index,
-            idCourse: `${course.maKhoaHoc}`,
-            title: `${course.tenKhoaHoc}`,
-            description: `${course.moTa}`,
-            img: `${course.hinhAnh}`,
-            rating: `${course.danhGia}`,
-        })
-    });
-
-    return (
-        <List
-            className='courseContent__enrolled mt-5'
-            itemLayout="vertical"
-            size="large"
-            pagination={{
-                onChange: (page) => {
-                    console.log(page);
-                },
-                pageSize: 3,
-            }}
-            dataSource={arrCourses}
-            renderItem={(item) => (
-                <List.Item
-                    key={item.title}
-                    actions={[
-                        <Progress percent={RANDOM_NUM + item.rating * 10} format={(percent) => `${percent}% complete`} />,
-                        <Rate allowHalf defaultValue={item.rating / 2} />
-                    ]}
-                    extra={
-                        <img
-                            width={272}
-                            alt="logo"
-                            src={item.img}
-                        />
-                    }
-                >
-                    <List.Item.Meta
-                        title={<NavLink to={`/detail/${item.idCourse}`}>{item.title}</NavLink>}
-                        description={item.description}
-                    />
-                    {item.content}
-                </List.Item>
-            )}
-        />
-    )
+  return (
+    <List
+      className="courseContent__enrolled mt-5"
+      itemLayout="vertical"
+      size="large"
+      pagination={{
+        onChange: (page) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      }}
+      dataSource={arrCourses}
+      renderItem={(item) => (
+        <List.Item
+          key={item.title}
+          actions={[
+            <Progress
+              percent={RANDOM_NUM + item.rating * 10}
+              format={(percent) => `${percent}% complete`}
+            />,
+            <Rate allowHalf defaultValue={item.rating / 2} />,
+            <div className="block">
+              <ButtonStyled
+                onClick={() => {
+                  cancelCourse(item.idCourse);
+                }}
+              >
+                Hủy khóa học
+              </ButtonStyled>
+            </div>,
+          ]}
+          extra={<img width={272} alt="logo" src={item.img} />}
+        >
+          <List.Item.Meta
+            title={
+              <NavLink to={`/detail/${item.idCourse}`}>{item.title}</NavLink>
+            }
+            description={item.description}
+          />
+          {item.content}
+        </List.Item>
+      )}
+    />
+  );
 }
