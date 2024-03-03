b4224676092f747e246f13646d5eaf10e14874a2
KHA0410 <khanguyen151121@gmail.com>
2024-03-03 23:09:38 +0700

    fix update

--- src/pages/AdminPage/CourseManagement/FormUpdateCourse.jsx
+++ src/pages/AdminPage/CourseManagement/FormUpdateCourse.jsx
@@ -13,17 +13,22 @@ import { ButtonStyled } from "../../../components/ButtonStyled/ButtonStyled";
 import { https } from "../../../services/api";
 import axios from "axios";
 import { TOKEN_CYBERSOFT } from "../../../services/constant";
+import TextArea from "antd/es/input/TextArea";
 
 export default function FormUpdateCourse({ record }) {
-  const [courseUpdate, setCourseUpdate] = useState(record);
-  const [category, setCategory] = useState([]);
-  const [options, setOptions] = useState([]);
+  console.log("record", record.danhMucKhoaHoc.maDanhMucKhoahoc);
+  const [form] = Form.useForm();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // const [courseUpdate, setCourseUpdate] = useState(record);
+  // const [category, setCategory] = useState([]);
+  // const [options, setOptions] = useState([]);
+
   const [imageURL, setImageURL] = useState(record.hinhAnh);
-  console.log("courseUpdate", courseUpdate);
 
-  let dataJson = JSON.parse(localStorage.getItem("USER_LOGIN"));
-  const { TextArea } = Input;
-
+  useEffect(() => {
+    form.setFieldValue(record);
+  }, [record, form]);
   // todo: get category list
   // const getCategory = () => {
   //   https
@@ -51,15 +56,15 @@ export default function FormUpdateCourse({ record }) {
   // }, []);
 
   //Chuyển đổi tên file hình ảnh
-  const normFile = (e) => {
-    if (Array.isArray(e)) {
-      return e;
-    }
-    const fileList = e && e.fileList;
-    const fileName = fileList && fileList.length > 0 && fileList[0].name;
+  // const normFile = (e) => {
+  //   if (Array.isArray(e)) {
+  //     return e;
+  //   }
+  //   const fileList = e && e.fileList;
+  //   const fileName = fileList && fileList.length > 0 && fileList[0].name;
 
-    return fileName;
-  };
+  //   return fileName;
+  // };
 
   // todo: handle change for image upload
   /* const handleImageChange = async (info) => {
@@ -94,33 +99,39 @@ export default function FormUpdateCourse({ record }) {
   }; */
 
   // todo: handle change for form
-  const handleChange = (name, value) => {
-    // update nested object if name == "danhMucKhoaHoc"
-    if (name === "danhMucKhoaHoc") {
-      setCourseUpdate({
-        ...courseUpdate,
-        danhMucKhoaHoc: {
-          maDanhMucKhoahoc: value,
-        },
-      });
-    } else {
-      // for other fields
-      setCourseUpdate({
-        ...courseUpdate,
-        [name]: value,
-      });
-    }
-  };
+  // const handleChange = (name, value) => {
+  //   // update nested object if name == "danhMucKhoaHoc"
+  //   if (name === "danhMucKhoaHoc") {
+  //     setCourseUpdate({
+  //       ...courseUpdate,
+  //       danhMucKhoaHoc: {
+  //         maDanhMucKhoahoc: value,
+  //       },
+  //     });
+  //   } else {
+  //     // for other fields
+  //     setCourseUpdate({
+  //       ...courseUpdate,
+  //       [name]: value,
+  //     });
+  //   }
+  // };
 
-  const onFinish = () => {
+  const onFinish = (values) => {
+    setIsSubmitting(true);
+    console.log("values", values);
     https
-      .put("/api/QuanLyKhoaHoc/CapNhatKhoaHoc", courseUpdate)
+      .put("/api/QuanLyKhoaHoc/CapNhatKhoaHoc", values)
       .then((res) => {
         console.log("res", res.data);
-        setCourseUpdate(res.data);
+        message.success("Cập nhập thành công");
+        setTimeout(function () {
+          window.location.reload();
+        }, 500);
       })
       .catch((err) => {
         console.log("err", err);
+        message.error(err.respone.data);
       });
   };
 
@@ -171,9 +182,9 @@ export default function FormUpdateCourse({ record }) {
         style={{
           maxWidth: 600,
         }}
-        initialValues={{
-          remember: true,
-        }}
+        initialValues={record}
+        form={form}
+        name="basic"
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
         autoComplete="off"
@@ -181,54 +192,79 @@ export default function FormUpdateCourse({ record }) {
         <div className="grid grid-cols-2 lg:grid-cols-1 gap-5">
           <div>
             {/* Mã khóa học */}
-            <Form.Item
-              label="Mã khóa học"
-              // name="maKhoaHoc"
-            >
-              <Input name="maKhoaHoc" value={courseUpdate.maKhoaHoc} disabled />
+            <Form.Item label="Mã khóa học" name="maKhoaHoc">
+              {/* <Input name="maKhoaHoc" value={courseUpdate.maKhoaHoc} disabled /> */}
+              <Input disabled />
             </Form.Item>
 
             {/*Tên khóa học */}
             <Form.Item
               label="Tên khóa học"
-              // name="tenKhoaHoc"
+              name="tenKhoaHoc"
+              rules={[
+                {
+                  required: true,
+                  message: "Vui lòng nhập mã khóa học!",
+                },
+              ]}
             >
-              <Input
+              {/* <Input
                 name="tenKhoaHoc"
                 value={courseUpdate.tenKhoaHoc}
                 onChange={(e) => {
                   handleChange("tenKhoaHoc", e.target.value);
                 }}
-              />
+              /> */}
+              <Input />
             </Form.Item>
 
-            {/* Danh mục khóa học */}
+            {/* Mã danh mục khóa học */}
             <Form.Item
               label="Danh mục khóa học"
-              // name="maDanhMucKhoahoc"
+              name="maDanhMucKhoahoc"
+              rules={[
+                {
+                  required: true,
+                  message: "Vui lòng chọn lại danh mục khóa học để update",
+                },
+              ]}
             >
               <Select
-                name="maDanhMucKhoahoc"
-                options={options}
-                value={courseUpdate.danhMucKhoaHoc?.maDanhMucKhoahoc}
-                onChange={(value) => {
-                  handleChange("danhMucKhoaHoc", value);
-                }}
-              ></Select>
+                // value={record.danhMucKhoaHoc.maDanhMucKhoahoc}
+                placeholder={record.danhMucKhoaHoc.tenDanhMucKhoaHoc}
+              >
+                <Select.Option value="BackEnd">Lập trình Backend</Select.Option>
+                <Select.Option value="Design">Thiết kế Web</Select.Option>
+                <Select.Option value="DiDong">Lập trình di động</Select.Option>
+                <Select.Option value="FrontEnd">
+                  Lập trình Front end
+                </Select.Option>
+                <Select.Option value="FullStack">
+                  Lập trình Full Stack
+                </Select.Option>
+                <Select.Option value="TuDuy">Tư duy lập trình</Select.Option>
+              </Select>
             </Form.Item>
 
             {/* Mã nhóm học*/}
             <Form.Item
               label="Mã nhóm học"
-              // name="maNhom"
+              name="maNhom"
+              rules={[
+                {
+                  required: true,
+                  message: "Vui lòng chọn nhóm học!",
+                },
+              ]}
             >
-              <Select
+              {/* <Select
                 name="maNhom"
                 value={courseUpdate.maNhom}
                 onChange={(value) => {
                   handleChange("maNhom", value);
                 }}
-              >
+              > */}
+              <Select>
                 <Select.Option value="GP01">GP01</Select.Option>
                 <Select.Option value="GP02">GP02</Select.Option>
                 <Select.Option value="GP03">GP03</Select.Option>
@@ -250,13 +286,20 @@ export default function FormUpdateCourse({ record }) {
             {/* Nguời tạo */}
             <Form.Item
               label="Người tạo"
-              // name="nguoiTao"
+              rules={[
+                {
+                  required: true,
+                  message: "Người tạo phải là GV!",
+                },
+              ]}
             >
               <Select
-                name="nguoiTao"
+                name="taiKhoanNguoiTao"
                 value="GV"
                 options={[{ value: "GV" }]}
-              ></Select>
+              >
+                {/* <Select.Option value={dataJson.taiKhoan}>GV</Select.Option> */}
+              </Select>
             </Form.Item>
           </div>
 
@@ -264,28 +307,32 @@ export default function FormUpdateCourse({ record }) {
             {/* Ngày tạo */}
             <Form.Item
               label="Ngày tạo"
-              // name="ngayTao"
+              name="ngayTao"
+              rules={[
+                {
+                  required: true,
+                  message: "Ngày tạo không được để trống!",
+                },
+              ]}
             >
-              <Input
+              {/* <Input
                 name="ngayTao"
                 value={courseUpdate.ngayTao}
                 onChange={(e) => {
                   handleChange("ngayTao", e.target.value);
                 }}
-              />
+              /> */}
+              <Input />
             </Form.Item>
 
             {/* Đánh giá */}
-            <Form.Item
-              label="Đánh giá"
-              // name="danhGia"
-            >
+            <Form.Item label="Đánh giá" name="danhGia">
               <InputNumber
-                name="danhGia"
+                // name="danhGia"
                 defaultValue="0"
-                onChange={(value) => {
-                  handleChange("danhGia", value);
-                }}
+                // onChange={(value) => {
+                //   handleChange("danhGia", value);
+                // }}
               />
             </Form.Item>
 
@@ -295,29 +342,40 @@ export default function FormUpdateCourse({ record }) {
                 width: "100%",
               }}
               label="Lượt xem"
-              // name="luotXem"
+              name="luotXem"
+              rules={[
+                {
+                  required: true,
+                  message: "Lượt xem không được để trống!",
+                },
+              ]}
             >
-              <InputNumber
+              {/* <InputNumber
                 name="luotXem"
                 value={courseUpdate.luotXem}
                 onChange={(value) => {
                   handleChange("luotXem", value);
                 }}
-              />
+              /> */}
+              <InputNumber />
             </Form.Item>
 
             {/* Mô tả */}
             <Form.Item
               label="Mô tả khóa học"
-              // name="moTa"
+              name="moTa"
+              rules={[
+                {
+                  required: true,
+                  message: "Mô tả không được để trống!",
+                },
+              ]}
             >
               <TextArea
-                name="moTa"
                 rows={4}
-                value={courseUpdate.moTa}
-                onChange={(e) => {
-                  handleChange("moTa", e.target.value);
-                }}
+                // onChange={(e) => {
+                //   handleChange("moTa", e.target.value);
+                // }}
               />
             </Form.Item>
 
