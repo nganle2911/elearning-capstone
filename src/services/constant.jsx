3af68a4b3aa230f37d478c3cf57d1792e82dbcfb
KHA0410 <khanguyen151121@gmail.com>
2024-03-18 22:48:22 +0700

    exp

--- src/services/constant.jsx
+++ src/services/constant.jsx
@@ -1,4 +1,5 @@
 export const MIN = 0;
 export const MAX = 100;
-export let RANDOM_NUM = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN; 
-export const TOKEN_CYBERSOFT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1OCIsIkhldEhhblN0cmluZyI6IjExLzA2LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxODA2NDAwMDAwMCIsIm5iZiI6MTY5MDM5MDgwMCwiZXhwIjoxNzE4MjExNjAwfQ.631rl3EwTQfz6CuufNTJlys36XLVmoxo29kP-F_PDKU";
+export let RANDOM_NUM = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+export const TOKEN_CYBERSOFT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1OCIsIkhldEhhblN0cmluZyI6IjE2LzA2LzIxMDAiLCJIZXRIYW5UaW1lIjoiMTcxODQ5NjAwMDAwMCIsIm5iZiI6MTY5MDM5MDgwMCwiZXhwIjoxNzE4NjQzNjAwfQ.nxKt-C-T3PtIxGCb0bAxvC-QEsYFLkZN1s3RQqW86Wc";
